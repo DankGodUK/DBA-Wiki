4e4ca5e8c26de32be8dab803d6e2c99e9d664630
DankGodUK <craigpferns@gmail.com>
2026-05-21 22:02:51 +0100

    Update fetch-changelog.js

--- .github/scripts/fetch-changelog.js
+++ .github/scripts/fetch-changelog.js
@@ -60,8 +60,6 @@ async function run() {
             author: m.author.username,
             date: m.timestamp,
             content: m.content
-                .replace(/```/g, '')
-                .trim()
         }));
 
     changelog.unshift(...newEntries.reverse());
